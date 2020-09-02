de6f27a31fdd2d8e08cc39e3ec114ac8a51194d8
Stacey P Carrillo <staceycarrillo44@gmail.com>
2020-09-02 19:35:56 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,5 +4,18 @@ end
 
 my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
 more_favs = ["sports cars", "flatiron school"]
-    
+  
+def using_insert(array, element)  
 
+list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
+another_language = "Python"
+new_array = using_insert(list_of_programming_languages, another_language)
+
+  it "takes in two arguments, an array and a new element to be added to the array. it uses the insert method to add the new element to the 4th index of the array" do 
+    expect(@new_array[4]).to eq(@another_language)
+  end
+
+  it "increases the length of the array" do 
+    expect(@new_array.length).to eq(8)
+  end
+end
