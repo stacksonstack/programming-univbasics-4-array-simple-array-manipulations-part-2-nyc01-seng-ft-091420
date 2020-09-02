e2f0025288c8234f217dfd62a77f3d56c9e89b8b
Stacey P Carrillo <staceycarrillo44@gmail.com>
2020-09-02 19:41:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,4 +14,11 @@ another_language = "Python"
 new_array = using_insert(list_of_programming_languages, another_language)
 
 
+def using_uniq(array)
+  
+end
+
+haircuts = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
+new_array = using_uniq(haircuts)
+
 
