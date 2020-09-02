a6289ca8a748500da4c1388f197e24d92ba64b87
Stacey P Carrillo <staceycarrillo44@gmail.com>
2020-09-02 19:33:43 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,3 +1,8 @@
 def using_concat(array1, array2) 
   array1.concat(array2)
 end
+
+my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+more_favs = ["sports cars", "flatiron school"]
+    
+
