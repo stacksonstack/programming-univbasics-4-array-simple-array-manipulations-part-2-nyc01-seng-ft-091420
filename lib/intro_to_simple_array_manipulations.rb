bfeb44eeb0407f38acad52a98a3ee90e2f8b6c47
Stacey P Carrillo <staceycarrillo44@gmail.com>
2020-09-02 19:42:21 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -5,13 +5,6 @@ end
 my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
 more_favs = ["sports cars", "flatiron school"]
   
-def using_insert(array, element)
-  array.insert(element)
-end
-
-list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
-another_language = "Python"
-new_array = using_insert(list_of_programming_languages, another_language)
 
 
 def using_uniq(array)
