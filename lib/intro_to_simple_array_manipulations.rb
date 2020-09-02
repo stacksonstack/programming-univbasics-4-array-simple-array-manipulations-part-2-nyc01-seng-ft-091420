d1a6c8fa6933bf9963028b5dc6d9eb3611256cb9
Stacey P Carrillo <staceycarrillo44@gmail.com>
2020-09-02 19:44:03 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -5,14 +5,6 @@ end
 my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
 more_favs = ["sports cars", "flatiron school"]
   
-def using_insert(array, element)
-  array.insert(element)
-end
-
-list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
-another_language = "Python"
-new_array = using_insert(list_of_programming_languages, another_language)
-
 
 def using_uniq(array)
   array.uniq
@@ -23,10 +15,9 @@ new_array = using_uniq(haircuts)
 
 
 def using_flatten(array)
-
-instruments = ["Saxophone", ["Piano", "Trumpet"], "Violin", "Drums", "Flute"]
-     flat_array = using_flatten(instruments)
-     expect(flat_array).to eq(["Saxophone", "Piano", "Trumpet", "Violin", "Drums", "Flute"])
-  end
+  array.flatten
 end
 
+instruments = ["Saxophone", ["Piano", "Trumpet"], "Violin", "Drums", "Flute"]
+flat_array = using_flatten(instruments)
+     
