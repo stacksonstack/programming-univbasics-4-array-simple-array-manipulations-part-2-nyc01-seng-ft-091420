f7c9abb27977e96fbc8c1baa29ee967e12528b96
Stacey P Carrillo <staceycarrillo44@gmail.com>
2020-09-02 19:51:12 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,25 +2,16 @@ def using_concat(array1, array2)
   array1.concat(array2)
 end
 
-my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-more_favs = ["sports cars", "flatiron school"]
-  
+
 
 def using_uniq(array)
   array.uniq
 end
 
-haircuts = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
-new_array = using_uniq(haircuts)
-
 
 def using_flatten(array)
   array.flatten
 end
-
-instruments = ["Saxophone", ["Piano", "Trumpet"], "Violin", "Drums", "Flute"]
-flat_array = using_flatten(instruments)
-
 
 
 
@@ -30,10 +21,3 @@ flat_array = using_flatten(instruments)
 def using_insert(array, element)
   array.insert(4, element)
 end
-
-list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
-another_language = "Python"
-new_array = using_insert(list_of_programming_languages, another_language)
-
-
-     
